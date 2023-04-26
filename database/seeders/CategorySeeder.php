97bb7052d1e629bb11731c6956de2f7105d0a226
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-26 18:33:41 +0200

    FEAT seeder cat done

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['Frontend','Backend','Fullstack','Design','Gestionali','Videogiochi'];
+
+        // ciclo array con categorie
+        foreach($categories as $cat) {
+            $new_cat = new Category();
+            $new_cat->name = $category_name;
+            $new_cat->slug = Str::slug($category_name);
+
+            $new_cat->save();
+        }
+
+
     }
 }
