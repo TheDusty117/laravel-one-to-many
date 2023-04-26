746498f2bd2c4e88b03d80d71cf7f24ae4d568f4
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-26 18:46:12 +0200

    FEAT categories created with migration

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use App\Models\Category;
 
 
@@ -19,7 +20,7 @@ class CategorySeeder extends Seeder
         $categories = ['Frontend','Backend','Fullstack','Design','Gestionali','Videogiochi'];
 
         // ciclo array con categorie
-        foreach($categories as $cat) {
+        foreach($categories as $category_name) {
             $new_cat = new Category();
             $new_cat->name = $category_name;
             $new_cat->slug = Str::slug($category_name);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UserSeeder::class,
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
