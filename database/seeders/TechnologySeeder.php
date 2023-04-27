1af0bcb789080da6cc62f38e76440f443d1dc8a7
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-27 14:51:27 +0200

    FEAT model created, migration and seeder & params

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+//importo model e Str!!!
+use App\Models\Technology;
+use Illuminate\Support\Str;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['CSS','SASS','Bootstrap', 'HTML','JavaScript','PHP','Vue','SQL','Laravel','Vite'];
+
+        foreach($technologies as $technology_name){
+            $technology = new Tech();
+            $technology->name = $technology_name;
+            $technology->slug = Str::slug($technology_name);
+            $technology->save();
+        }
+
+    }
+}
